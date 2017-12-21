f393005c762031d70f6a73282ef17c3a64dd568f
Aaron <Aarones2015@gmail.com>
2017-12-20 19:29:03 -0500

    hw-88

---
+++ hw-88/myTube.js
@@ -0,0 +1,31 @@
+(function () {
+    "use strict";
+    var myTubeList = $("#myTubeList"),
+        // myTubeListUl = $("#myTubeList ul li"),
+        myTube = $("#myTube video"),
+        errorElement = $("#errorElement"),
+        storedVideos = "",
+        value = 0;
+
+    $.get("jTube.json", function (loadedText) {
+        storedVideos = loadedText;
+        console.log(storedVideos);
+        storedVideos.forEach(display);
+        play();
+    }).fail(function (jqxhr, status, statusText) {
+        errorElement.text("Failed to load file: " + jqxhr.status + " " + statusText);
+        errorElement.show();
+    })
+    function display(storedVideos) {
+        $("#myTubeList ul").append('<li value="' + (value++) + '">' + storedVideos.Title + "</button></li>");
+
+    }
+    function play() {
+        // myTubeListUl.click(function (event) {
+        $("#myTubeList ul li").click(function (event) {
+            console.log(this.value);
+            console.log(event);
+            myTube.attr('src', storedVideos[this.value].url);
+        })
+    }
+})()
