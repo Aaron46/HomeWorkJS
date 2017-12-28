36f23444387efd2e9f45261cd9f2be2668aff166
Aaron <Aarones2015@gmail.com>
2017-12-27 20:23:01 -0500

    hw-90

---
+++ hw-86/getContacts.php
@@ -0,0 +1,11 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+require 'db.php';
+
+$query = "SELECT * FROM contacts";
+$stmt = $db->query($query);
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//print_r($rows);
+echo json_encode($rows);
+?>
